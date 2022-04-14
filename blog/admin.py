5e71bba804645a761aabcdf5009bee0778e246da
Koval Bohdan [PC] <workbohdankoval@gmail.com>
2022-04-14 15:56:18 +0300

    Made a blog page

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'publish','status')
-    list_filter = ('status', 'created', 'publish', 'author')
-    search_fields = ('title', 'body')
+    list_display = ('title', 'slug', 'author', 'publish',
+                    'status')  # Поля которые нужно показывать
+    list_filter = ('status', 'created', 'publish', 'author')  # Фильтрация
+    search_fields = ('title', 'body')  # Поиск
+    # Автоматически заполнение слага
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
-    ordering = ('status', 'publish')
+    ordering = ('status', 'publish')  # Метод сортировки
