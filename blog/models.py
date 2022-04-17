516dcaebb7daf59819c51720f9233634ec19b7ab
Koval Bohdan [PC] <workbohdankoval@gmail.com>
2022-04-17 14:37:45 +0300

    Made a system of recommendations for the article (by tags)

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -19,6 +20,7 @@ class Post(models.Model):
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
+    tags = TaggableManager()
 
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
--- blog/views.py
+++ blog/views.py
@@ -3,24 +3,29 @@ from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 from blog.forms import EmailPostForm
 from .models import Post, Comment
 
 
-# Create your views here.
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginator_by = 3
+    paginate_by = 2
     template_name = 'blog/post/list.html'
 
 
-"""
-Post list на обработчике функции 
+# Post list на обработчике функции
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)  # По 3 статьи на каждой странице.
     page = request.GET.get('page')
     try:
@@ -30,15 +35,42 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         # Если номер страницы больше, чем общее кол-чи страниц, возвращаем последнюю.
-        posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
-"""
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
-                             publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    post = get_object_or_404(
+        Post, slug=post, status='published', publish__year=year,
+        publish__month=month, publish__day=day)
+    # Список активных комментариев для этой статьи.
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # Пользователь отправил комментарий.
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Создаем комментарий, но не сохраняем в базе данных
+            new_comment = comment_form.save(commit=False)
+            # Привязываем комментарий к текущей статье
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    # Доп. функционал Формирование списка похожих статей.
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                     'comments': comments,
+                                                     'new_comment': new_comment,
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
@@ -65,27 +97,3 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
-
-
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-        Post, slug=post, status='published', publish__year=year,
-        publish__month=month, publish__day=day)
-    # Список активных комментариев для этой статьи.
-    comments = post.comments.filter(active=True)
-    new_comment = None
-    if request.method == 'POST':
-        # Пользователь отправил комментарий.
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            # Создаем комментарий, но не сохраняем в базе данных
-            new_comment = comment_form.save(commit=False)
-            # Привязываем комментарий к текущей статье
-            new_comment.post = post
-            new_comment.save()
-    else:
-        comment_form = CommentForm()
-    return render(request, 'blog/post/detail.html', {'post': post,
-                                                     'comments': comments,
-                                                     'new_comment': new_comment,
-                                                     'comment_form': comment_form})
